539ec9690a4721d35d7ea75b28cacba937e9fe61
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-04-14 14:16:48 +0200

    Simplify the nix code

--- flake.nix
+++ flake.nix
@@ -31,39 +31,25 @@
             ];
           };
           checks = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
-          packages = {
-            default = self'.packages.humble-linear-feedback-controller-msgs;
-            humble-linear-feedback-controller-msgs =
-              pkgs.rosPackages.humble.linear-feedback-controller-msgs.overrideAttrs
-                {
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [
-                      ./CMakeLists.txt
-                      ./include
-                      ./linear_feedback_controller_msgs_py
-                      ./msg
-                      ./package.xml
-                      ./tests
-                    ];
-                  };
-                };
-            jazzy-linear-feedback-controller-msgs =
-              pkgs.rosPackages.jazzy.linear-feedback-controller-msgs.overrideAttrs
-                {
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [
-                      ./CMakeLists.txt
-                      ./include
-                      ./linear_feedback_controller_msgs_py
-                      ./msg
-                      ./package.xml
-                      ./tests
-                    ];
-                  };
-                };
-          };
+          packages = let
+              src = lib.fileset.toSource {
+                root = ./.;
+                fileset = lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./include
+                  ./linear_feedback_controller_msgs_py
+                  ./msg
+                  ./package.xml
+                  ./tests
+                ];
+              };
+            in {
+              default = self'.packages.humble-linear-feedback-controller-msgs;
+              humble-linear-feedback-controller-msgs =
+                pkgs.rosPackages.humble.linear-feedback-controller-msgs.overrideAttrs { inherit src; };
+              jazzy-linear-feedback-controller-msgs =
+                pkgs.rosPackages.jazzy.linear-feedback-controller-msgs.overrideAttrs { inherit src; };
+            };
           treefmt.programs = {
             deadnix.enable = true;
             nixfmt.enable = true;
