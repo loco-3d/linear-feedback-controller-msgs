a217b53a271e9c0b9c8831cd7906d25c50ed34ca
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-24 19:01:57 +0200

    flake: add overlay

--- flake.nix
+++ flake.nix
@@ -12,35 +12,48 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        { lib, pkgs, ... }:
-        {
-          packages =
-            let
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./include
-                  ./linear_feedback_controller_msgs_py
-                  ./msg
-                  ./package.xml
-                  ./tests
-                ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, self, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          { gepetto-pkgs.overlays = [ self.overlays.default ]; }
+        ];
+        flake.overlays.default =
+          _final: prev:
+          let
+            scope = _ros-final: ros-prev: {
+              linear-feedback-controller-msgs = ros-prev.linear-feedback-controller-msgs.overrideAttrs {
+                src = lib.fileset.toSource {
+                  root = ./.;
+                  fileset = lib.fileset.unions [
+                    ./CMakeLists.txt
+                    ./include
+                    ./linear_feedback_controller_msgs_py
+                    ./msg
+                    ./package.xml
+                    ./tests
+                  ];
+                };
               };
-            in
-            lib.filterAttrs (_n: v: v.meta.available && !v.meta.broken) (rec {
+            };
+          in
+          {
+            rosPackages = prev.rosPackages // {
+              humble = prev.rosPackages.humble.overrideScope scope;
+              jazzy = prev.rosPackages.jazzy.overrideScope scope;
+            };
+          };
+        perSystem =
+          { pkgs, ... }:
+          {
+            packages = lib.filterAttrs (_n: v: v.meta.available && !v.meta.broken) (rec {
               default = humble-linear-feedback-controller-msgs;
-              humble-linear-feedback-controller-msgs =
-                pkgs.rosPackages.humble.linear-feedback-controller-msgs.overrideAttrs
-                  { inherit src; };
-              jazzy-linear-feedback-controller-msgs =
-                pkgs.rosPackages.jazzy.linear-feedback-controller-msgs.overrideAttrs
-                  { inherit src; };
+              humble-linear-feedback-controller-msgs = pkgs.rosPackages.humble.linear-feedback-controller-msgs;
+              jazzy-linear-feedback-controller-msgs = pkgs.rosPackages.jazzy.linear-feedback-controller-msgs;
             });
-        };
-    };
+          };
+      }
+    );
 }
