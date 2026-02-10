6eb4dbba6a480529c8a39dc5f6f684e8c5c1e801
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-10 18:18:39 +0100

    nix: switch to gazebros2nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "ROS messages which correspond to the loco-3d/linear-feedback-controller package.";
 
   inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
+    gazebros2nix.url = "github:gepetto/gazebros2nix";
+    flake-parts.follows = "gazebros2nix/flake-parts";
+    nixpkgs.follows = "gazebros2nix/nixpkgs";
+    nix-ros-overlay.follows = "gazebros2nix/nix-ros-overlay";
+    systems.follows = "gazebros2nix/systems";
+    treefmt-nix.follows = "gazebros2nix/treefmt-nix";
   };
 
   outputs =
@@ -17,8 +17,8 @@
       {
         systems = import inputs.systems;
         imports = [
-          inputs.gepetto.flakeModule
-          { gepetto-pkgs.overlays = [ self.overlays.default ]; }
+          inputs.gazebros2nix.flakeModule
+          { gazebros2nix-pkgs.overlays = [ self.overlays.default ]; }
         ];
         flake.overlays.default =
           _final: prev:
@@ -43,15 +43,19 @@
             rosPackages = prev.rosPackages // {
               humble = prev.rosPackages.humble.overrideScope scope;
               jazzy = prev.rosPackages.jazzy.overrideScope scope;
+              kilted = prev.rosPackages.kilted.overrideScope scope;
+              rolling = prev.rosPackages.rolling.overrideScope scope;
             };
           };
         perSystem =
           { pkgs, ... }:
           {
             packages = lib.filterAttrs (_n: v: v.meta.available && !v.meta.broken) (rec {
-              default = humble-linear-feedback-controller-msgs;
+              default = rolling-linear-feedback-controller-msgs;
               humble-linear-feedback-controller-msgs = pkgs.rosPackages.humble.linear-feedback-controller-msgs;
               jazzy-linear-feedback-controller-msgs = pkgs.rosPackages.jazzy.linear-feedback-controller-msgs;
+              kilted-linear-feedback-controller-msgs = pkgs.rosPackages.kilted.linear-feedback-controller-msgs;
+              rolling-linear-feedback-controller-msgs = pkgs.rosPackages.rolling.linear-feedback-controller-msgs;
             });
           };
       }
